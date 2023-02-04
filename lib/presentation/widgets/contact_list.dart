64f84e614e23df8296b0a444b51dac6fc0bb554d
Imnmania <niloyb@live.com>
2023-02-04 19:29:28 +0600

    fixed divider indentation on contact list

--- lib/presentation/widgets/contact_list.dart
+++ lib/presentation/widgets/contact_list.dart
@@ -12,7 +12,15 @@ class ContactList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView.builder(
+      child: ListView.separated(
+        separatorBuilder: (context, index) {
+          return const Divider(
+            indent: 88,
+            endIndent: 6,
+            height: 0,
+            color: dividerColor,
+          );
+        },
         itemCount: info.length,
         itemBuilder: (context, index) {
           final item = info[index];
@@ -23,34 +31,31 @@ class ContactList extends StatelessWidget {
                   log('Tapped ${item['name']}');
                   onTap();
                 },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundImage:
-                          NetworkImage(item['profilePic']?.toString() ?? 'N/A'),
-                    ),
-                    title: Text(item['name']?.toString() ?? 'N/A'),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Text(item['message']?.toString() ?? 'N/A'),
-                    ),
-                    trailing: Text(
-                      item['time']?.toString() ?? 'N/A',
-                      style: const TextStyle(
-                        color: Colors.grey,
-                        fontSize: 13,
-                      ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundImage:
+                        NetworkImage(item['profilePic']?.toString() ?? 'N/A'),
+                  ),
+                  title: Text(item['name']?.toString() ?? 'N/A'),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(item['message']?.toString() ?? 'N/A'),
+                  ),
+                  trailing: Text(
+                    item['time']?.toString() ?? 'N/A',
+                    style: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 13,
                     ),
                   ),
                 ),
               ),
-              const Divider(
-                indent: 95,
-                endIndent: 10,
-                color: dividerColor,
-              ),
+              // const Divider(
+              //   indent: 95,
+              //   endIndent: 10,
+              //   color: dividerColor,
+              // ),
             ],
           );
         },
