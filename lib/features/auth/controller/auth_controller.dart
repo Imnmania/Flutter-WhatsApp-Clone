6cc00a7b67ff1afb211fe640b12502dae826d11d
Imnmania <niloyb@live.com>
2023-04-07 22:38:23 +0600

    added riverpod

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone/features/auth/repository/auth_repository.dart';
+
+final authControllerProvider = Provider((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AuthController(authRepository: authRepository);
+});
+
+class AuthController {
+  final AuthRepository authRepository;
+
+  const AuthController({
+    required this.authRepository,
+  });
+
+  void signInWithPhone({
+    required BuildContext context,
+    required String phoneNumber,
+  }) {
+    authRepository.signInWithPhone(
+      context: context,
+      phoneNumber: phoneNumber,
+    );
+  }
+}
---
+++ lib/features/auth/repository/auth_repository.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone/common/utils/snackbar_util.dart';
+import 'package:whatsapp_clone/features/auth/screens/otp_screen.dart';
+
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    auth: FirebaseAuth.instance,
+    firestore: FirebaseFirestore.instance,
+  ),
+);
+
+class AuthRepository {
+  final FirebaseAuth auth;
+  final FirebaseFirestore firestore;
+
+  const AuthRepository({
+    required this.auth,
+    required this.firestore,
+  });
+
+  Future<void> signInWithPhone({
+    required BuildContext context,
+    required String phoneNumber,
+  }) async {
+    try {
+      await auth.verifyPhoneNumber(
+        phoneNumber: phoneNumber,
+        verificationCompleted: (phoneAuthCredential) async {
+          await auth.signInWithCredential(phoneAuthCredential);
+        },
+        verificationFailed: (error) {
+          throw Exception(error.message);
+        },
+        codeSent: (verificationId, forceResendingToken) async {
+          Navigator.pushNamed(
+            context,
+            OTPScreen.routeName,
+            arguments: verificationId,
+          );
+        },
+        codeAutoRetrievalTimeout: (verificationId) {
+          // do something when needed
+        },
+      );
+    } on FirebaseAuthException catch (ex) {
+      showSnackbar(
+        context: context,
+        content: ex.message!,
+      );
+    }
+  }
+}
